dc151896615c21c721f03fcbbdb5996ecbc6304c
Tushar-sketch-bit <maliktushar749@gmail.com>
2025-07-18 22:43:17 +0530

    Auth module-1

--- first/src/main/java/com/tusharcodes/first/config/JwtService.java
+++ first/src/main/java/com/tusharcodes/first/config/JwtService.java
@@ -34,10 +34,20 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public boolean isTokenValid(String token, String userEmail) {
+        final String username = extractUsername(token);
+        return (username.equals(userEmail)) && !isTokenExpired(token);
+    }
 
-
-
-
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
 
 
